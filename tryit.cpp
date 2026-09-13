9e383b834a84e1882c0e7f0359540cb29ab71d0e
colonelkarani <quavix0000@gmail.com>
2026-09-13 20:44:01 +0300

    proj

--- tryit.cpp
+++ tryit.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
+#include <vector>
 #include "raylib.h"
 #include "VKUtils.h"
 
 using namespace std;
 
+class Circle
+{
+public:
+Vector2 position;
+int radius;
+Color color;
+Circle(Vector2 position, int radius, Color color): position(position), radius(radius), color(color){}
+};
+
 int main ()
 {
+    InitWindow(1000,1000,"My fucking circles");
     cout<<"A random number is: "<<GetRandomInt(10,90)<<endl;
+    Circle my_circle({100,100}, 30, YELLOW); 
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(GREEN);
+        if (IsMouseButtonPressed(0))
+        {
+            DrawCircle(my_circle.position.x, my_circle.position.y, my_circle.radius, my_circle.color);
+        }
+        
+        DrawText("Must code today", 0,0,20,BLACK);
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
