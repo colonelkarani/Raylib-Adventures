3f3017417ca9dd2bdf3aa55d1741eae4df09cb3b
colonelkarani <quavix0000@gmail.com>
2026-09-25 00:16:34 +0300

    A little bit complicated than i thought

--- poker.cpp
+++ poker.cpp
@@ -1,11 +1,39 @@
 #include <raylib.h>
+#include <vector>
+
+
+typedef enum Suit
+{
+SPADES,
+CLUBS,
+DIAMONDS,
+HEARTS
+};
+
+typedef enum Rank { Ace = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King };
+
+class Card
+{
+
+public:
+    int width;
+    int height;
+    Vector2 position;
+    Suit suit;
+
+    Card(/* args */);
+    ~Card();
+};
+
 
 int main()
 {
     InitWindow(500,500,"Jr is cool");
 while (!WindowShouldClose())
 {
-    /* code */
+    BeginDrawing();
+    ClearBackground(BLUE);
+    EndDrawing();
 }
 
 
